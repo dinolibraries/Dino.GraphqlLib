478960634433b47135d4b8cb7c602366b8341bc9
Unmatched TaiNguyen <tainguyen.ntt.97@gmail.com>
2024-01-13 08:34:02 +0700

    add GrapqlControllerBase

---
+++ Dino.GraphqlLib/Controllers/GraphqlContentResult.cs
@@ -0,0 +1,26 @@
+﻿using EntityGraphQL.AspNet;
+using EntityGraphQL;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Dino.GraphqlLib.Controllers
+{
+    public class GraphqlContentResult : IActionResult
+    {
+        private readonly QueryResult _results;
+        public GraphqlContentResult(QueryResult keyValuePairs)
+        {
+            _results = keyValuePairs;
+        }
+        public async Task ExecuteResultAsync(ActionContext context)
+        {
+            await context.HttpContext.RequestServices
+               .GetService<IGraphQLResponseSerializer>().SerializeAsync(context.HttpContext.Response.Body, _results);
+        }
+    }
+}
---
+++ Dino.GraphqlLib/Controllers/GrapqlControllerBase.cs
@@ -0,0 +1,38 @@
+﻿using EntityGraphQL.Schema;
+using EntityGraphQL;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Dino.GraphqlLib.Controllers
+{
+    public class GrapqlControllerBase : ControllerBase
+    {
+        protected async Task<IActionResult> TryOk(Func<Task<IActionResult>> func)
+        {
+            try
+            {
+                return await func();
+            }
+            catch (Exception ex)
+            {
+                ModelState.AddModelError(string.Empty, ex.Message);
+                return BadRequest(ModelState);
+            }
+        }
+        protected async Task<IActionResult> RequestGraphql<TContext>(QueryRequest query)
+        {
+            var context = HttpContext.RequestServices.GetService<TContext>();
+            var schemeProvider = HttpContext.RequestServices.GetService<SchemaProvider<TContext>>();
+            var results = await schemeProvider.ExecuteRequestWithContextAsync(query, context, HttpContext.RequestServices, HttpContext.User);
+            var errors = results.Errors;
+            if (errors != null && errors!.Count > 0)
+            {
+                return BadRequest(errors);
+            }
+            else
+            {
+                return new GraphqlContentResult(results);
+            }
+        }
+    }
+}
